0f4a938a07b91d09c06a67070f39f4a0cdfdfa2f
Tim Havlicek <t.havlicek@s-v.de>
2022-06-18 17:45:40 +0200

    handle file names

--- src/web/src/actions/handleFiles.ts
+++ src/web/src/actions/handleFiles.ts
@@ -4,6 +4,28 @@ import { log } from "../log";
 
 import * as FFMPEG from "@ffmpeg/ffmpeg";
 
+function getNameTemplate(name, template = "####") {
+  return name.replace(/[0-9]+/g, template);
+}
+
+function firstFrameOfSeq(seq: string[]) {
+  let min = Infinity;
+  for (const file of seq) {
+    try {
+      if (file) {
+        const f = file.match(/[0-9]+/g);
+        if (f) {
+          const n = parseInt(f.toString());
+          min = Math.min(n, min);
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  return min;
+}
+
 async function convertFiles(files) {
   const ffmpeg = FFMPEG.createFFmpeg({
     corePath: "/ffmpeg-core.js",
@@ -13,15 +35,27 @@ async function convertFiles(files) {
     },
   });
 
-  const extension = files[0].name.split(".").reverse()[0];
+  const template = files[0].name.match(/[0-9]+/g);
+  let padding = 4;
+  if (template) {
+    padding = template.toString().length;
+  }
+
+  const fileEntry = getNameTemplate(files[0].name, `%0${padding}d`);
+  const firstFrame = firstFrameOfSeq([...files].map((file) => file.name))
+    .toString()
+    .padStart(padding, "0");
+
+  log(`Converting files ${fileEntry} from frame ${firstFrame}.`);
+  console.log(`Converting files ${fileEntry} at frame ${firstFrame}.`);
 
   // ffmpeg -apply_trc iec61966_2_1 -start_number 1100 -i input%04d.exr output.mp4
 
   const args = [
     "-i",
-    `%04d.${extension}`,
+    fileEntry,
     "-start_number",
-    "0001",
+    firstFrame,
     "-pix_fmt",
     "yuv420p",
     "-vcodec",
