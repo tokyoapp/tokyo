9c5132b103dceecb1ae6f7dd86b97e6f65adf5fc
Tim Havlicek <t.havlicek@s-v.de>
2022-06-18 02:22:35 +0200

    firebase

---
+++ src/web/src/components/FileDropzone.ts
@@ -0,0 +1,6 @@
+import { LitElement } from "lit";
+
+// drop files and dispatch an event with said file's hanldes
+// eg a sequence of PNG's, also parse sequence range into single object of mutliple files
+
+LitElement;
