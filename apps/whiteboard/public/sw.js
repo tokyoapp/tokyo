b93dec7d701af4fdc9fa329ec0f05a1b1cdab168
luckydye <tim.h4vlicek@gmail.com>
2023-02-12 02:05:35 +0100

    clean

--- apps/whiteboard/public/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', function (event) {
-    // console.log(event.request.url);
-
-    event.respondWith(
-        caches.match(event.request).then(function (response) {
-            return response || fetch(event.request);
-        })
-    );
-});
