24ace466fea9cf13988ce2485a21cf360155b20e
luckydye <tim.h4vlicek@gmail.com>
2023-02-12 02:05:35 +0100

    clean

--- apps/evo/vite.config.ts
+++ apps/evo/vite.config.ts
@@ -2,22 +2,17 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 import wrapWorker from "vite-plugin-wrap-worker";
 import svgSprites from "svg-sprites/vite";
-import Package from "./package.json";
-
-const envVars = {
-  __PACKAGE__: {
-    version: Package.version,
-    name: Package.name,
-    description: Package.description,
-  },
-};
+import path from "node:path";
 
 export default defineConfig({
-  base: "",
   publicDir: "static",
   server: {
     port: 3000,
   },
-  define: envVars,
+  // resolve: {
+  //   alias: {
+  //     "~": path.resolve("./"),
+  //   },
+  // },
   plugins: [solidPlugin(), wrapWorker(), svgSprites()],
 });
