7e91ee9b358d9ad201fa898fbc0d863cc44f652e
luckydye <tim.h4vlicek@gmail.com>
2023-03-24 19:49:41 +0100

    feat: desktop app

---
+++ apps/_desktop/src/App.tsx
@@ -0,0 +1,38 @@
+import { createSignal } from "solid-js";
+import { invoke } from "@tauri-apps/api/tauri";
+import "./App.css";
+
+function App() {
+  const [greetMsg, setGreetMsg] = createSignal("");
+  const [name, setName] = createSignal("");
+
+  async function greet() {
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    setGreetMsg(await invoke("greet", { name: name() }));
+  }
+
+  return (
+    <div class="container">
+      <h1>Welcome to Tauri!</h1>
+
+      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
+
+      <div class="row">
+        <div>
+          <input
+            id="greet-input"
+            onChange={(e) => setName(e.currentTarget.value)}
+            placeholder="Enter a name..."
+          />
+          <button type="button" onClick={() => greet()}>
+            Greet
+          </button>
+        </div>
+      </div>
+
+      <p>{greetMsg}</p>
+    </div>
+  );
+}
+
+export default App;
