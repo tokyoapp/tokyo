f3e46da0c069eb1233e9c31eb702f7dc2fc4278f
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-08-27 13:39:14 +0200

    update tauri init

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -1,18 +1,10 @@
-#![cfg_attr(
-    all(not(debug_assertions), target_os = "windows"),
-    windows_subsystem = "windows"
-)]
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::Manager;
-
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn show_main_window(window: tauri::Window) {
-    window.get_window("main").unwrap().show().unwrap();
 }
 
 fn main() {
--- apps/desktop/src/App.tsx
+++ apps/desktop/src/App.tsx
@@ -1,4 +1,5 @@
 import { createSignal } from "solid-js";
+import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
@@ -15,22 +16,36 @@ function App() {
     <div class="container">
       <h1>Welcome to Tauri!</h1>
 
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
       <div class="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
-        </div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
+        </a>
+        <a href="https://tauri.app" target="_blank">
+          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
+        </a>
+        <a href="https://solidjs.com" target="_blank">
+          <img src={logo} class="logo solid" alt="Solid logo" />
+        </a>
       </div>
 
-      <p>{greetMsg}</p>
+      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
+
+      <form
+        class="row"
+        onSubmit={(e) => {
+          e.preventDefault();
+          greet();
+        }}
+      >
+        <input
+          id="greet-input"
+          onChange={(e) => setName(e.currentTarget.value)}
+          placeholder="Enter a name..."
+        />
+        <button type="submit">Greet</button>
+      </form>
+
+      <p>{greetMsg()}</p>
     </div>
   );
 }
