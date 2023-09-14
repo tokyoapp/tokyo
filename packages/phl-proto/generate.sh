044d55c754a6b9c20b5fdb2853d865c9c6740294
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-09-14 22:01:53 +0200

    codegen is ts

--- packages/phl-proto/generate.sh
+++ packages/phl-proto/generate.sh
@@ -8,7 +8,7 @@ echo generating code from $SRC_DIR/$NAME.proto ...
 
 protoc --proto_path=$SRC_DIR --rust_out=$DST_DIR $SRC_DIR/$NAME.proto
 
-bun x pbjs -t static-module -w es6 -o $DST_DIR/$NAME.js $SRC_DIR/$NAME.proto
-bun x pbts -o $DST_DIR/$NAME.d.ts $DST_DIR/$NAME.js
+bun x pbjs -t static-module -w es6 -o $DST_DIR/$NAME.ts $SRC_DIR/$NAME.proto
+bun x pbts -o $DST_DIR/$NAME.d.ts $DST_DIR/$NAME.ts
 
 echo done
