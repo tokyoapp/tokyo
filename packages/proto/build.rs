7ee34b39a18511d8637a86ae78c15bf8ea4be190
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-10-05 23:59:07 +0200

    log proto dist path

--- packages/proto/build.rs
+++ packages/proto/build.rs
@@ -3,35 +3,35 @@ use std::path::PathBuf;
 use std::process::Command;
 
 fn main() -> std::io::Result<()> {
-    let include = PathBuf::from("src/protos");
-    let input = PathBuf::from("src/protos/library.proto");
-    let dist = PathBuf::from("src/gen");
+  let include = PathBuf::from("src/protos");
+  let input = PathBuf::from("src/protos/library.proto");
+  let dist = PathBuf::from("src/gen");
 
-    println!("Generating code from {:?}", input);
+  println!("Generating code from {:?} tp {:?}", input, dist.display());
 
-    if !dist.exists() {
-        fs::create_dir_all(dist.clone())?;
-    }
+  if !dist.exists() {
+    fs::create_dir_all(dist.clone())?;
+  }
 
-    Command::new("protoc")
-        .args([
-            "-I",
-            include.to_str().unwrap(),
-            "--ts_proto_out",
-            dist.to_str().unwrap(),
-            input.to_str().unwrap(),
-        ])
-        .output()
-        .expect("failed to execute process");
+  Command::new("protoc")
+    .args([
+      "-I",
+      include.to_str().unwrap(),
+      "--ts_proto_out",
+      dist.to_str().unwrap(),
+      input.to_str().unwrap(),
+    ])
+    .output()
+    .expect("failed to execute process");
 
-    Command::new("protoc")
-        .args([
-            "--rust_out",
-            dist.to_str().unwrap(),
-            input.to_str().unwrap(),
-        ])
-        .output()
-        .expect("failed to execute process");
+  Command::new("protoc")
+    .args([
+      "--rust_out",
+      dist.to_str().unwrap(),
+      input.to_str().unwrap(),
+    ])
+    .output()
+    .expect("failed to execute process");
 
-    Ok(())
+  Ok(())
 }
