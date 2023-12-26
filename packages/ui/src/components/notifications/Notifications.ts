8ebaf9266e855d13ab83a16d33da71abb8d0e410
Tim Havlicek <tim.h4vlicek@gmail.com>
2023-12-26 23:29:39 +0100

    feat: notification log

--- packages/ui/src/components/notifications/Notifications.ts
+++ packages/ui/src/components/notifications/Notifications.ts
@@ -2,13 +2,25 @@ import ErrorNotification from './ErrorNotification.js';
 import Notification from './Notification.js';
 import NotificationFeed from './NotificationFeed.js';
 
+const log: Notification[] = [];
+
 export class Notifications {
-  static push(notification: Notification) {
+  private static push(notification: Notification) {
     const feed = NotificationFeed.getInstance();
     if (feed) {
       feed?.append(notification);
+      log.unshift(notification);
       return notification;
     }
+  }
+
+  static info(message: string) {
+    Notifications.push(
+      new Notification({
+        message: `${message}`,
+        time: 3000,
+      })
+    );
   }
 
   static error(message: string) {
